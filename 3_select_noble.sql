208c10489d3d41ce63f9c5c5507d55a2108b892e
Kingsley <kingstevens007@gmail.com>
2019-04-06 12:38:32 +0100

    Update 3_select_noble.sql

--- 3_select_noble.sql
+++ 3_select_noble.sql
@@ -65,7 +65,7 @@ WHERE winner = 'PETER GRÜNBERG'
 -- Find all details of the prize won by EUGENE O'NEILL
 SELECT *
 FROM nobel
-WHERE winner = "EUGENE O'NEILL"
+WHERE winner = 'EUGENE O''NEILL'
 
 -- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
 SELECT winner, yr, subject 
